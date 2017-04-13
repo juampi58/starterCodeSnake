abf8ad525b1fa5c7c4ce68109da4f39ac70b118a
Juampi <juanpabloloza@gmail.com>
2017-04-13 10:40:52 +0200

    make snake and add print snake method

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 function Game(options){
   this.rows=options.rows;
   this.columns=options.columns;
+  this.snake= options.snake;
 
 for(var rowIndex=0 ; rowIndex<this.rows; rowIndex++){
   for(var columnIndex=0 ; columnIndex<this.columns; columnIndex++){
@@ -10,14 +11,16 @@ for(var rowIndex=0 ; rowIndex<this.rows; rowIndex++){
              .attr('data-row',rowIndex)
              .attr('data-column',columnIndex));
 
-
-
-
+    }
   }
 }
 
-}
-
+Game.prototype.drawSnake=function(){
+  this.snake.body.forEach(function(position,index){
+    var selector = '[data-row=' + position.row +'][data-column=' + position.column +']';
+    $(selector).addClass('snake');
+  });
+};
 
 
 
@@ -26,6 +29,7 @@ $(document).ready(function () {
 var game= new Game({
   rows:50,
   columns:50,
+  snake: new Snake()
 });
-
+game.drawSnake();
 });
---
+++ js/snake.js
@@ -0,0 +1,12 @@
+function Snake(){
+  this.body= [
+    {row:1,column:5},
+    {row:1,column:4},
+    {row:1,column:3},
+    {row:1,column:2},
+    {row:1,column:1},
+  ];
+
+  this.direction='right';
+
+}
